d79c0aee74f98e1dea11c3b7d8fa0856b72cb05a
Antti Takalahti <antti.takalahti@gmail.com>
2015-04-08 18:43:13 +0300

    added sort to calculations

--- data/replace_numbers_with_x_and_group.sh
+++ data/replace_numbers_with_x_and_group.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-cat data1.txt| sed -E 's/([1-9][0-9])/x/g'|sed -E 's/[1-9]/x/g'|sort|uniq -c > patterns.txt
+cat data1.txt| sed -E 's/([1-9][0-9])/x/g'|sed -E 's/[1-9]/x/g'|sort|uniq -c|sort > patterns_asc.txt
 
