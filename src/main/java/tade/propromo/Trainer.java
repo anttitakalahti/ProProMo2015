bb098b9dad40f1219d1ee000e8d723d4de9f037d
Antti Takalahti <antti.takalahti@gmail.com>
2015-04-18 11:48:03 +0300

    added another round files

--- src/main/java/tade/propromo/Trainer.java
+++ src/main/java/tade/propromo/Trainer.java
@@ -16,15 +16,18 @@ import java.util.HashSet;
 
 public class Trainer {
 
-    public static final String TRAINING_DATA_FILE_NAME = "data2.csv";
-    public static final int TRAINING_DATA_ROWS = 68760;
+    public static final String TRAINING_DATA_FILE_NAME_ROUND_ONE = "data1.csv";
+    public static final String TRAINING_DATA_FILE_NAME_ROUND_TWO = "data2.csv";
+
+    public static final int TRAINING_DATA_ROWS_ROUND_ONE = 67785;
+    public static final int TRAINING_DATA_ROWS_ROUND_TWO = 68760;
 
     private static final int REPORTING_SCALE = 6;
 
     public static void main(String[] args) throws Exception {
 
 
-        int[][] testData = initializeTestDataFromFile(2000);
+        int[][] testData = initializeTestDataFromFile(TRAINING_DATA_FILE_NAME_ROUND_TWO, 2000);
 
         BigDecimal baseline = runWithWorker(new TrainingDataWorker(testData, new DuvinsPredictor()));
         BigDecimal currentBestPredictor = runWithWorker(new TrainingDataWorker(testData, Worker.MY_BEST_PREDICTOR));
@@ -47,15 +50,23 @@ public class Trainer {
         return worker.calculateScore();
     }
 
-    public static int[][] initializeTestDataFromFile() throws IOException {
-        return initializeTestDataFromFile(TRAINING_DATA_ROWS);
+    public static int[][] initializeTestDataFromFile(String fileName) throws IOException {
+        int rows = TRAINING_DATA_ROWS_ROUND_TWO;
+        if (fileName.equals(TRAINING_DATA_FILE_NAME_ROUND_ONE)) {
+            rows = TRAINING_DATA_ROWS_ROUND_ONE;
+        }
+        return initializeTestDataFromFile(fileName, rows);
     }
 
-    public static int[][] initializeTestDataFromFile(int rows) throws IOException {
-        HashSet<Integer> indexes = getRandomIndexes(rows);
+    public static int[][] initializeTestDataFromFile(String fileName, int rows) throws IOException {
+        int totalRows = TRAINING_DATA_ROWS_ROUND_TWO;
+        if (fileName.equals(TRAINING_DATA_FILE_NAME_ROUND_ONE)) {
+            totalRows = TRAINING_DATA_ROWS_ROUND_ONE;
+        }
+        HashSet<Integer> indexes = getRandomIndexes(rows, totalRows);
 
         int[][] testData = new int[rows][];
-        File file = new File(Trainer.class.getClassLoader().getResource(TRAINING_DATA_FILE_NAME).getFile());
+        File file = new File(Trainer.class.getClassLoader().getResource(fileName).getFile());
         FileReader fr = new FileReader(file);
         BufferedReader br = new BufferedReader(fr);
         String line;
@@ -72,11 +83,11 @@ public class Trainer {
         return testData;
     }
 
-    public static HashSet<Integer> getRandomIndexes(int rows) {
+    public static HashSet<Integer> getRandomIndexes(int rows, int totalRows) {
         HashSet<Integer> indexes = new HashSet<>(rows);
-        if (rows < TRAINING_DATA_ROWS) {
-            ArrayList<Integer> validIndexes = new ArrayList<>(TRAINING_DATA_ROWS);
-            for(int i=0; i<TRAINING_DATA_ROWS; ++i) {
+        if (rows < totalRows) {
+            ArrayList<Integer> validIndexes = new ArrayList<>(totalRows);
+            for(int i=0; i<totalRows; ++i) {
                 validIndexes.add(i);
             }
             Collections.shuffle(validIndexes);
