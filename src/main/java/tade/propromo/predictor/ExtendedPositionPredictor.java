3bc854390a35b176e124fc3f497cc84ccc81333d
Antti Takalahti <antti.takalahti@gmail.com>
2015-04-20 00:30:12 +0300

    now with less hassle

--- src/main/java/tade/propromo/predictor/ExtendedPositionPredictor.java
+++ src/main/java/tade/propromo/predictor/ExtendedPositionPredictor.java
@@ -8,24 +8,23 @@ import java.util.HashSet;
 
 public class ExtendedPositionPredictor implements Predictor {
 
-    private static final double MY_MINIMAL = 0.0001;
-
-    private int[][] trainingData;
-
     private double[][] preCalculatedProbabilities;
-
 
     public ExtendedPositionPredictor() {
         try {
-
-            trainingData = Trainer.initializeTestDataFromFile(Trainer.TRAINING_DATA_FILE_NAME_ROUND_TWO);
-
+            int[][] trainingData = Trainer.initializeTestDataFromFile(Trainer.TRAINING_DATA_FILE_NAME_ROUND_TWO);
 
             preCalculatedProbabilities = new double[303][];
             for (Integer position=0; position<COLS; ++position) {
-                preCalculatedProbabilities[position] = getValueProbabilitiesForPosition(position);
-            }
+                int[] counts = new int[100];
+                Arrays.fill(counts, 10);
 
+                for(int[] line : trainingData) {
+                    counts[line[position]]++;
+                }
+
+                preCalculatedProbabilities[position] = normalize(counts);
+            }
         } catch (IOException e) {
             e.printStackTrace(System.out);
             System.exit(-1);
@@ -34,29 +33,16 @@ public class ExtendedPositionPredictor implements Predictor {
 
     @Override
     public double[] getFirstGuess() {
-        return predictPosition(0, new int[0]);
+        return predictPosition(0);
     }
 
     @Override
     public double[] predictRow(int round, int[] previousValues) {
-        return predictPosition(round, previousValues);
+        return predictPosition(round);
     }
 
-    protected double[] predictPosition(int position, int[] previousValues) {
+    protected double[] predictPosition(int position) {
         return preCalculatedProbabilities[position];
-    }
-
-
-
-    private double[] getValueProbabilitiesForPosition(Integer position) {
-        int[] counts = new int[100];
-        Arrays.fill(counts, 10);
-
-        for(int[] line : trainingData) {
-            counts[line[position]]++;
-        }
-
-        return normalize(counts);
     }
 
 }
